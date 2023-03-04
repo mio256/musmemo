e5f1bfa953802f21ebb01da8c916cc7e599a0261
mio256 <reiya@woorth.io>
2023-03-04 23:36:56 +0900

    :bug: ESLint:Missing "key" prop for element in iterator

--- pages/index.js
+++ pages/index.js
@@ -78,7 +78,7 @@ export default function Home() {
             </form>
           )}
           {Object.values(data).map((item) => (
-            <a
+            <a key={index} 
               href="#"
               onClick={() => removeTodo(item)}
               className={styles.card}
